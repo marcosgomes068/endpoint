c5f54f5b9b3d9979d647cb9fb21e9511420328e9
marcosgomes <marcosgabrielgomes105@gmail.com>
2025-06-07 08:37:56 -0500

    Delete api directory

--- api/webhook-mp.js
+++
@@ -1,9 +0,0 @@
-// endpoint/api/webhook-mp.js
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('Recebido webhook Mercado Pago:', req.body);
-    res.status(200).json({ ok: true });
-  } else {
-    res.status(200).send('Webhook Mercado Pago ativo!');
-  }
-}
